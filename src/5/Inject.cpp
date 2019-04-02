cb5e5337675449421cb5103a51fb60725e290c46
lycheng <18087603176@163.com>
2019-04-02 22:30:44 +0800

    第五次作业提交

---
+++ src/5/Inject.cpp
@@ -0,0 +1,71 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+
+int main() {
+	char szDllName[] = "C:\\Users\\HP\\source\\repos\\HookWriteFile\\Debug\\HookWriteFile.dll";
+	char szExeName[] = "notepad.exe";
+
+	// 读取进程列表，获得目标进程的PID
+	PROCESSENTRY32 ProcessEntry = {};
+	ProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	bool bRet = Process32First(hProcessSnap, &ProcessEntry);
+	DWORD dwProcessId = 0;
+	while (bRet) {
+		if (strcmp(szExeName, ProcessEntry.szExeFile) == 0) {
+			dwProcessId = ProcessEntry.th32ProcessID;
+			break;
+		}
+		bRet = Process32Next(hProcessSnap, &ProcessEntry);
+	}
+	if (0 == dwProcessId) {
+		printf("找不到进程\n");
+		return 1;
+	}
+
+	// 利用PID获得进程句柄
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
+	if (0 == hProcess) {
+		printf("无法打开进程\n");
+		return 1;
+	}
+
+	// 在进程当中去分配空间
+	size_t length = strlen(szDllName) + 1;
+	char * pszDllFile = (char *)VirtualAllocEx(hProcess, NULL, length, MEM_COMMIT, PAGE_READWRITE);
+	if (0 == pszDllFile) {
+		printf("远程空间分配失败\n");
+		return 1;
+	}
+
+	// 将函数的参数写到进程空间中去
+	if (!WriteProcessMemory(hProcess, (PVOID)pszDllFile, (PVOID)szDllName, length, NULL)) {
+		printf("远程空间写入失败\n");
+		return 1;
+	}
+
+	// 获取LoadLibraryA函数
+	PTHREAD_START_ROUTINE pfnThreadRtn = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("kernel32"), "LoadLibraryA");
+	if (0 == pfnThreadRtn) {
+		printf("LoadLibraryA函数地址获取失败\n");
+		return 1;
+	}
+
+	// 在目标进程中根据函数地址和参数创建进程
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, pfnThreadRtn, (PVOID)pszDllFile, 0, NULL);
+	if (0 == hThread) {
+		printf("远程线程创建失败\n");
+		return 1;
+	}
+
+	// 等待进程执行完毕
+	WaitForSingleObject(hThread, INFINITE);
+	printf("远程线程执行完毕!\n");
+
+	VirtualFreeEx(hProcess, (PVOID)pszDllFile, 0, MEM_RELEASE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return 0;
+}
